6451fabaafa2edc1a89965ba95275454ca539b53
Emanuele Coppola <emanuele.coppola99@gmail.com>
2021-06-13 15:00:11 +0200

    Add resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
-require('./bootstrap');
+// Shared functions
+function debounce(func, timeout = 300){
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+}
+
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', debounce(() => {
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}), 400);
+
