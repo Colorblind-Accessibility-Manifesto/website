bf0b617c5c8c95c196336b9543dc7053ba62c48d
Emanuele Coppola <emanuele.coppola99@gmail.com>
2021-06-13 15:00:54 +0200

    Add web routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
+
+Route::get('/issues', function () {
+    return view('issues');
+})->name('issues');
